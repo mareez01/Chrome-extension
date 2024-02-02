54de1c6af3975b22fdf5efad6759121c8cb0965c
mareez01 <siva22012005@gmail.com>
2024-02-02 08:59:29 +0530

    chnage

--- background.js
+++ background.js
@@ -1,8 +1,21 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === 'sendToGoogleSheets') {
-      // Implement logic to send data to Google Sheets (e.g., using Google Sheets API or Google Apps Script)
-      // This might involve making an HTTP request to your Apps Script web app endpoint
-      console.log('Data received in background.js:', request.data);
+      var data = request.data;
+  
+      // Specify your Google Apps Script web app URL
+      var scriptUrl = 'https://script.google.com/macros/s/AKfycbz8Gq8fmWzePFJYa6YowVxODTpKYlGcMjzKtN-Yw-mfa_jqfTmInLQ1Z6FfYgpZ96Fn/exec';
+  
+      // Send data to Google Sheets using POST request
+      fetch(scriptUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.error('Error sending data to Google Sheets:', error));
     }
   });
   
