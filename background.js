5802359cb9732ba95ae1ded04d1b26f22c3864fe
mareez01 <siva22012005@gmail.com>
2024-02-05 15:21:18 +0530

    changed

--- background.js
+++ background.js
@@ -1,41 +1,43 @@
 // background.js
 
 // Listener for messages from content scripts or other parts of the extension
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log(message)
+// background.js
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  console.log(message);
+
   if (message.type === 'extractedData') {
     const dataArray = message.data;
-    console.log(dataArray)
-    console.log('started')
+    console.log(dataArray);
+    console.log('started');
+
     // Check if data is extracted
     if (dataArray.length > 0) {
-      // Send the extracted data to the server
-      const id = 'AKfycbyJHcUiHjo6Alm4IgnIq5bWGYcTcL9fpQyutLP0rDJGcY7hbTfVVAjO4eGONekFEc7Arw'
-      const url = `https://script.google.com/macros/s/${id}/exec`
-        // Sending a POST request using fetch
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ data: dataArray }),
-    })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(result => {
-    console.log('Data sent successfully:', result);
-  })
-  .catch(error => {
-    console.error('Error during POST request:', error);
-    console.log('error happened')
-  });
+      const id = 'AAKfycbxYH41B0vimUGL8jgRQNRFbDU0GZO_jhONYCzzHmiElrIPEVmUlmZ_8gJ4SJDW5deHeUA';
+      const url = `https://script.google.com/macros/s/${id}/exec`;
+
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ data: dataArray }),
+        });
+        console.log(response)
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        //const result = await response.json();
+        console.log('Data sent successfully:', response);
+      } catch (error) {
+        console.error('Error during POST request:', error);
+        console.log('Error happened');
+      }
     } else {
       console.log('No data extracted');
     }
   }
 });
-
--- popup.js
+++ popup.js
@@ -1,76 +1,84 @@
 // popup.js
+
+// Trigger data extraction when the extension button is clicked
+document.getElementById('extractData').addEventListener("click", extractData);
+function extractData(){
+  document.getElementById('extractData').removeEventListener("click", extractData);
+  // Execute content script to extract data
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      func: injectScript
+    })
+})
+}
+
 var element;
 var position;
 var link;
 var connection;
 var emailsInContainer ;
 let dataArray;
-// Trigger data extraction when the extension button is clicked
-document.getElementById('extractData').addEventListener("click", extractData);
-function extractData(){
-  // Execute content script to extract data
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      func: () => {
-        element = document.querySelector('.text-heading-xlarge.inline.t-24.v-align-middle.break-words').textContent;
-        //position = document.querySelector('.text-body-medium.break-words').textContent;
-        link=window.location.href;
-        dataArray =[element, link]
-        try {
-          connection = document.querySelector('.t-normal.t-black--light.t-14.hoverable-link-text');
-          if (connection) {
-            connection = connection.textContent.trim();
-            if (connection && connection.toLowerCase().includes('animesh singhal')) {
-              if (connection.toLowerCase().includes('connection')){
-              console.log('Connection:', connection);
-              dataArray.push('Y');
-            }} else {
-              dataArray.push('N');
-              console.log('Connection not matching:', connection);
-            }
-          } else {
-            dataArray.push('N');
-            console.log('Connection element not found');
-          }
-        } catch (error) {
-          console.error('Error:', error.message);
-        }
-        
-        //mails
-        try{
-          emailContainers = document.querySelectorAll('.pv-contact-info__ci-container.t-14');
-          console.log(emailContainers)
-          function extractEmails(text) {
-            var regex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
-            return text.match(regex) || [];
-          }
-          // Loop through each email container
-           emailContainers.forEach(function(container) {
-            // Get the text content of the container
-            var containerText = container.innerText;
-          
-            // Extract email addresses from the container text
-            emailsInContainer = extractEmails(containerText);
-          
-            // Check if any email addresses were found
-            if (emailsInContainer.length > 0) {
-              console.log('Emails in container:', emailsInContainer[0]);
-              dataArray.push(emailsInContainer[0])
-              alert(`Mail Found`)
-            }
-            else{dataArray}
-          });
-        }catch(error){
-          console.error('Error:', error.message);
-        }
 
-        console.log(dataArray)
-        // Move the alert inside the asynchronous block
-        alert('Process Completed');
-        chrome.runtime.sendMessage({ type: 'extractedData', data: dataArray });
-      }
-    })
-})
+
+async function injectScript(){
+element = document.querySelector('.text-heading-xlarge.inline.t-24.v-align-middle.break-words').textContent;
+position = document.querySelector('.text-body-medium.break-words').textContent;
+link=window.location.href;
+if (position){
+  alert(`Position Value ${position}`)
 }
+dataArray =[element,position, link]
+try {
+  connection = document.querySelector('.t-normal.t-black--light.t-14.hoverable-link-text');
+  if (connection) {
+    connection = connection.textContent.trim();
+    if (connection && connection.toLowerCase().includes('animesh singhal')) {
+      if (connection.toLowerCase().includes('connection')){
+      console.log('Connection:', connection);
+      dataArray.push('Y');
+    }} else {
+      dataArray.push('N');
+      console.log('Connection not matching:', connection);
+    }
+  } else {
+    dataArray.push('N');
+    console.log('Connection element not found');
+  }
+} catch (error) {
+  console.error('Error:', error.message);
+}
+
+//mails
+try{
+  emailContainers = document.querySelectorAll('.pv-contact-info__ci-container.t-14');
+  console.log(emailContainers)
+  function extractEmails(text) {
+    var regex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+    return text.match(regex) || [];
+  }
+  // Loop through each email container
+   emailContainers.forEach(function(container) {
+    // Get the text content of the container
+    var containerText = container.innerText;
+  
+    // Extract email addresses from the container text
+    emailsInContainer = extractEmails(containerText);
+  
+    // Check if any email addresses were found
+    if (emailsInContainer.length > 0) {
+      console.log('Emails in container:', emailsInContainer[0]);
+      dataArray.push(emailsInContainer[0])
+      alert(`Mail Found`)
+    }
+    else{dataArray}
+  });
+}catch(error){
+  console.error('Error:', error.message);
+}
+
+console.log(dataArray)
+// Move the alert inside the asynchronous block
+alert('Process Completed');
+chrome.runtime.sendMessage({ type: 'extractedData', data: dataArray });}
 
